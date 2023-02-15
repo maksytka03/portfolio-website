4c97d772aa95a507144cfff1f4680c95b6a824fa
Pavlo <Pavlo>
2023-02-14 19:58:03 -0600

    Add description on the new columns

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ with col2:
     and this is my first website written on it.
     """
     st.info(content)
+
+content2 = """
+Below, you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
