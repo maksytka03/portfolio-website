667ebfaf5639c2b9d991af72f5fbf50d10977e7c
Pavlo <Pavlo>
2023-02-14 20:51:16 -0600

    Added description, image, and links

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas
 
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image("images/photo.jpeg", width=600)
@@ -28,7 +28,13 @@ df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write("[Source Code](https://python.org)")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write("[Source Code](https://python.org)")
