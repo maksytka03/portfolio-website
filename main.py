7b3f1bc6830d5042e933366b443e1a128d0c5797
Pavlo <Pavlo>
2023-02-14 19:34:59 -0600

    Create columns, image, and description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.jpeg", width=600)
+
+with col2:
+    st.title("Pavlo Demianov")
+    content = """
+    Hi, I am Pavlo! I am a Python programmer. Currently, I am actively learning Python,
+    and this is my first website written on it.
+    """
+    st.info(content)
